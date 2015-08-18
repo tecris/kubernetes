63402b1d087aac5991214694341bf0003cab555f
Eugen Cristea <eugen@cristea.org>
2015-08-18 15:41:00 +1200

    Update getk8.sh

--- getk8.sh
+++ getk8.sh
@@ -2,7 +2,7 @@
 VERSION=v1.0.1
 
 wget https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kube-controller-manager
-wget https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kube-api-server
+wget https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kube-apiserver
 wget https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kube-scheduler
 wget https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kube-proxy
 wget https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kubelet
