7f41f2f4cecb0a24f18f4798970f8dbedaf975bd
Eugen Cristea <eugen@cristea.org>
2015-12-29 10:41:02 +1300

    static ip address for nodes

--- coreos-libvirt/deploy_nodes_coreos_libvirt.sh
+++ coreos-libvirt/deploy_nodes_coreos_libvirt.sh
@@ -57,5 +57,5 @@ for SEQ in $(seq 1 $2); do
 
         sed "s#%HOSTNAME%#$COREOS_HOSTNAME#g;s#%DISCOVERY%#$ETCD_DISCOVERY#g" $LIBVIRT_PATH/node_user_data > $LIBVIRT_PATH/$COREOS_HOSTNAME/openstack/latest/user_data
 
-        virt-install --connect qemu:///system --import --name $COREOS_HOSTNAME --ram $RAM --vcpus $CPUs --os-type=linux --os-variant=virtio26 --disk path=$LIBVIRT_PATH/$COREOS_HOSTNAME.qcow2,format=qcow2,bus=virtio --filesystem $LIBVIRT_PATH/$COREOS_HOSTNAME/,config-2,type=mount,mode=squash --vnc --noautoconsole
+        virt-install --connect qemu:///system --import --name $COREOS_HOSTNAME --ram $RAM --vcpus $CPUs --os-type=linux --os-variant=virtio26 --disk path=$LIBVIRT_PATH/$COREOS_HOSTNAME.qcow2,format=qcow2,bus=virtio --filesystem $LIBVIRT_PATH/$COREOS_HOSTNAME/,config-2,type=mount,mode=squash --vnc --noautoconsole --network network=default,model=virtio,mac=52:54:00:2c:05:0$SEQ
 done
--- coreos-libvirt/staticIpAddress.sh
+++ coreos-libvirt/staticIpAddress.sh
@@ -7,3 +7,13 @@ virsh net-update default add ip-dhcp-host \
           "<host mac='52:54:00:2c:06:79' \
            name='master' ip='192.168.122.10' />" \
            --live --config
+
+virsh net-update default add ip-dhcp-host \
+          "<host mac='52:54:00:2c:05:01' \
+           name='node-01' ip='192.168.122.51' />" \
+           --live --config
+
+virsh net-update default add ip-dhcp-host \
+          "<host mac='52:54:00:2c:05:02' \
+           name='node-02' ip='192.168.122.52' />" \
+           --live --config
