7cd50b50aaeaa3e84e52e231e510a9b9db81a88a
Eugen Cristea <eugen@cristea.org>
2016-01-01 03:02:42 +1300

    update doc

--- coreos-libvirt/staticIpAddress.sh
+++ coreos-libvirt/staticIpAddress.sh
@@ -2,6 +2,7 @@
 
 # static ip address for master
 # http://serverfault.com/questions/627238/kvm-libvirt-how-to-configure-static-guest-ip-addresses-on-the-virtualisation-ho
+# http://wiki.libvirt.org/page/Networking#virsh_net-update
 
 virsh net-update default add ip-dhcp-host \
           "<host mac='52:54:00:2c:06:79' \
