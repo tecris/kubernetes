014e780d0617cd8b38a54e03c2dbde0c50e9d9fb
Eugen Cristea <eugen@cristea.org>
2016-01-29 12:54:07 +0100

    added to repo

---
+++ coreos-libvirt/installKubectl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+KUBERNETES_VERSION=v1.1.7
+
+wget https://storage.googleapis.com/kubernetes-release/release/$KUBERNETES_VERSION/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin
+
+# configuration
+kubectl config set-cluster default-cluster --server=http://192.168.122.10:8080
+kubectl config set-context default-system --cluster=default-cluster
+kubectl config use-context default-system
