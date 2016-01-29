df9e993e85cd5f33d3c7243c1743a9388ae21faa
Eugen Cristea <eugen@cristea.org>
2016-01-29 13:22:20 +0100

    update

--- coreos-libvirt/addons/dns/replace.sh
+++ coreos-libvirt/addons/dns/replace.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-export DNS_REPLICAS=1;
-export DNS_DOMAIN=cluster.local;
-export DNS_SERVER_IP=10.100.89.100;
-export KUBE_SERVER=192.168.122.10;
+DNS_REPLICAS=1;
+DNS_DOMAIN=cluster.local;
+DNS_SERVER_IP=10.100.89.100;
+KUBE_SERVER=192.168.122.10;
 
 sed -e "s/{{ pillar\['dns_replicas'\] }}/$DNS_REPLICAS/g;s/{{ pillar\['dns_domain'\] }}/$DNS_DOMAIN/g;s/{kube_server_url}/$KUBE_SERVER/g;" skydns-rc.yaml.in > ./skydns-rc.yaml
 
