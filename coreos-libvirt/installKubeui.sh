ad83e9a157ec5b63476c863b035d88b2bc45f542
Eugen Cristea <eugen@cristea.org>
2016-01-29 13:05:48 +0100

    added dns

--- coreos-libvirt/installKubeui.sh
+++ coreos-libvirt/installKubeui.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-KUBERNETES_VERSION=v1.1.7
 
 kubectl create -f addons/kube-system.yaml
+
+# ui addon
 kubectl --namespace=kube-system create -f addons/kube-ui/kube-ui-rc.yaml
 kubectl --namespace=kube-system create -f addons/kube-ui/kube-ui-svc.yaml
+
+# dns addon
+kubectl create -f addons/dns/skydns-rc.yaml
+kubectl create -f addons/dns/skydns-svc.yaml
