bcf310859ae0a0e8856b95a1d54297fa36ee5c06
Eugen Cristea <eugen@cristea.org>
2016-01-29 13:58:03 +0100

    update

--- coreos-libvirt/installAddons.sh
+++ coreos-libvirt/installAddons.sh
@@ -8,5 +8,6 @@ kubectl --namespace=kube-system create -f addons/kube-ui/kube-ui-rc.yaml
 kubectl --namespace=kube-system create -f addons/kube-ui/kube-ui-svc.yaml
 
 # dns addon
+cd addons/dns && ./replace.sh && cd ../..
 kubectl create -f addons/dns/skydns-rc.yaml
 kubectl create -f addons/dns/skydns-svc.yaml
