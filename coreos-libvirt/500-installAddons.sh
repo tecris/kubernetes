23234a7a87275329180f7a711af10e00136d77e9
Eugen Cristea <eugen@cristea.org>
2016-03-19 17:28:47 +0200

    removed ui addon

--- coreos-libvirt/500-installAddons.sh
+++ coreos-libvirt/500-installAddons.sh
@@ -3,10 +3,6 @@
 
 kubectl create -f addons/kube-system.yaml
 
-# ui addon
-kubectl --namespace=kube-system create -f addons/kube-ui/kube-ui-rc.yaml
-kubectl --namespace=kube-system create -f addons/kube-ui/kube-ui-svc.yaml
-
 # dns addon
 cd addons/dns && ./replace.sh && cd ../..
 kubectl create -f addons/dns/skydns-rc.yaml
